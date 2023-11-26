c4e4f856e94c3400110827739c2c2f7d069b7e34
MilosJovanovic1 <jovanovicm.198@gmail.com>
2023-11-26 14:32:43 +0100

    Initial commit

---
+++ src/test/java/resources/Credentials.java
@@ -0,0 +1,9 @@
+package resources;
+
+public class Credentials {
+    public static String firstName = "Milos";
+    public static String lastName = "Jovanovic";
+    public static String postalCode = "11500";
+    public static String userName = "standard_user";
+    public static String password = "secret_sauce";
+}
